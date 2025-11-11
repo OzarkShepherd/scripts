e18f6353a8778686e7caa9025aeba57d6858a67c
Scott Mackay <68165701+OzarkShepherd@users.noreply.github.com>
2025-11-11 16:05:16 -0600

    Add files via upload

---
+++ json-to-yml.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 -c "
+import yaml
+import json
+with open('python3-litellm.json', 'r') as f:
+    yaml_data = json.load(f)
+print(yaml.dump(yaml_data, sort_keys=False, default_flow_style=False))" > python3-litellm-2.yml
